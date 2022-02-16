3aaf05813d8f9698fd0fa4271dca99ac4aa0939c
Zalimkhan <zalimkhan@gmail.com>
2022-02-16 13:47:43 +0300

    Add files via upload

--- parse-logs.sh
+++ parse-logs.sh
@@ -2,7 +2,7 @@
 LOGDIR=$HOME'/log'
 CURRENT_MONTH=$(date +'%Y%m')
 CURRENT_DAY=$(date +'%Y%m%d')
-
+TOTAL_SUCCESS=0
 
 for FNAME in $LOGDIR'/*_'$CURRENT_MONTH'*.gz'; do
 	gzip -k -d -c  $FNAME | grep Error > $LOGDIR'/Error.log'
@@ -10,7 +10,8 @@ for FNAME in $LOGDIR'/*_'$CURRENT_MONTH'*.gz'; do
 done
 
 for FNAME in $LOGDIR'/*_'$CURRENT_DAY'*.gz'; do
-	gzip -k -d -c  $FNAME | grep -c Success 
+	SUCCESS=$(gzip -k -d -c  $FNAME | grep -c Success)
+	let "TOTAL_SUCCESS += $SUCCESS" 
 
 done
-
+echo "total count of success messages is $TOTAL_SUCCESS"
